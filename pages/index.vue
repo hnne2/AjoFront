46d4b7e0e91c58bf836b20a16a64b8516a49469f
Gri Makiev <makiev.grisha@gmail.com>
2025-06-16 14:04:14 +0300

    add politika

--- pages/index.vue
+++ pages/index.vue
@@ -91,7 +91,7 @@ if (data.value) {
         <div class="steps__stickers">
           <CardAnimate>
             <img
-                :src="`${baseUrl}/ajo/images/${data.stickers.image.url}`"
+                :src="`${baseUrl}/ajo/images/${data.stickers.images.url}`"
                 :alt="data.prizes.stickers.image.alt"
             />
             <div class="steps__stickers-info">
